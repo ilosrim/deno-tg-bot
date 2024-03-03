552b16fbd6f9cdefb043f09313c7bc3cae8a9589
ilosrim <ilosrim@yahoo.com>
2024-03-03 11:39:20 +0500

    first commit

--- core/bot.ts
+++ core/bot.ts
@@ -1,5 +1,10 @@
 import { Bot } from "https://deno.land/x/grammy@v1.21.1/mod.ts";
 import "https://deno.land/std@0.218.2/dotenv/load.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
 export const bot = new Bot(Deno.env.get("BOT_TOKEN"));
-bot.start();
+
+const app = new Application(); // or whatever you're using
+
+// Make sure to specify the framework you use.
+app.use(webhookCallback(bot, "oak"));
